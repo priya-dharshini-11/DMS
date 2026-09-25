1fc6cf5c01f07056fa8e32429f0418f719a033ac
Priyadharshini <priyadharshini07411@gmail.com>
2026-09-25 21:03:19 +0530

    Polish global UI and responsive vault header

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,13 @@
 console.log("Dead Man's Switch Vault loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".btn").forEach((button) => {
+        button.addEventListener("click", () => {
+            button.classList.add("dms-button-clicked");
+
+            setTimeout(() => {
+                button.classList.remove("dms-button-clicked");
+            }, 180);
+        });
+    });
+});
